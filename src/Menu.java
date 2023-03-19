abbca532c914dd14a53b3d86e3fdfeaadf574c49
kdykm <kdykm@DESKTOP-ND7MCLG>
2023-03-19 21:40:17 +0900

    Menu

---
+++ src/Menu.java
@@ -0,0 +1,13 @@
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("Menu:");
+		System.out.println("Add Item");
+		System.out.println("Delete Item");
+		System.out.println("Edit Item");
+		System.out.println("View Item");
+		System.out.println("Exit");
+	}
+
+}
