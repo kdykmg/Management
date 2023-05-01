312bcd879f70a933fe674e12452a15260ad13507
kdykm <kdykm@DESKTOP-ND7MCLG>
2023-05-01 23:28:45 +0900

    .

--- src/ItemMannerger.java
+++ src/ItemMannerger.java
@@ -1,55 +1,105 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ItemMannerger {
-	static Item[] item=new Item[10];
+	ArrayList<Item> itemList=new ArrayList<Item>();
 	Scanner in;
 	ItemMannerger(Scanner in){
 		this.in=in;
 	}
 	public void AddItems() {
-		System.out.println("Enter the Identification Number of Item:");
-		int itemNumber=in.nextInt();
-		System.out.println(itemNumber);
-		System.out.println("Enter the Item Name:");
-		String itemName=in.next();
-		System.out.println(itemName);
-		System.out.println("Enter the quantity of Item:");
-		int itemQuantity=in.nextInt();
-		System.out.println(itemQuantity);
-		item[itemNumber-1]=new Item(itemNumber,itemName,itemQuantity);
+		System.out.println("is Item is food? 1.Y/2.N");
+		int kind=0;
+		Item item;
+		while(kind!=1&&kind!=2) {
+			kind=in.nextInt();
+			if(kind==1) {
+				item=new FoodItem();
+				item.getInput(in);
+				itemList.add(item);
+				break;
+				
+			}
+			else {
+				item=new Item();
+				item.getInput(in);
+				itemList.add(item);
+				break;
+			}
+		}
+		
+		
 	}
 	public void DeleteItems() {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the Identification Number of Item:");
-		int itemNumber=in.nextInt();
-		item[itemNumber-1]=null;
-	}
-	public void EditItems() {
 		
 		System.out.println("Enter the Identification Number of Item:");
 		int itemNumber=in.nextInt();
-		System.out.println("Enter the Item Name:");
-		String itemName=in.next();
-		System.out.println("Enter the quantity of Item:");
-		int itemQuantity=in.nextInt();
-		item[itemNumber-1].edit(itemName,itemQuantity);
-	}
-	public void ViewItem() {
-		
-		System.out.println("Enter the Identification Number of Item:");
-		int itemNumber=in.nextInt();
-		item[itemNumber-1].viewItemNumber();
-		item[itemNumber-1].viewItemName();
-		item[itemNumber-1].viewItemQuantity();
-	}
-	public void ItemList() {
-		System.out.println("Item List:");
-		for(int i=0;i<10;i++) {
-			if(item[i]==null)continue;
-			item[i].viewItemNumber();
-			item[i].viewItemName();
-			item[i].viewItemQuantity();
-			System.out.println("");
+		int index=-1;
+		for(int i=0;i<itemList.size();i++) {
+			if(itemList.get(i).getItemNumber()==itemNumber) {
+				index=i;
+				break;
+			}
+		}
+		if(index>=0) {
+			itemList.remove(index);
+			System.out.println("Itemnumber"+itemNumber+"is delete");
+		}
+		else {
+			System.out.println("there no itemnumber");
+			return;
 		}
 	}
+	public void EditItems() {
+		System.out.println("Enter the Identification Number of Item:");
+		int itemNumber=in.nextInt();
+		int index=-1;
+		for(int i=0;i<itemList.size();i++) {
+			if(itemList.get(i).getItemNumber()==itemNumber) {
+				index=i;
+				break;
+			}
+		}
+		int num=-1;
+		while(num!=4) {
+			num=in.nextInt();
+			System.out.println("--Edit Menu--");
+			System.out.println("1.Item Number:");
+			System.out.println("2.Item Name:");
+			System.out.println("3.Quantity of Item:");
+			System.out.println("4.Exit");
+			switch(num) {
+			case 1:
+				System.out.println("Item Number:");
+				int number=in.nextInt();
+				itemList.get(index).setItemNumber(number);
+				break;
+			case 2:
+				System.out.println("Item Name:");
+				String name=in.next();
+				itemList.get(index).setItemName(name);
+				break;
+			case 3:
+				System.out.println("Quantity of Item:");
+				int quantity=in.nextInt();
+				itemList.get(index).setItemQuantity(quantity);
+				break;
+			}
+		}
+	}
+	public void ViewItem() {
+		System.out.println("Enter the Identification Number of Item:");
+		int itemNumber=in.nextInt();
+		int index=-1;
+		for(int i=0;i<itemList.size();i++) {
+			if(itemList.get(i).getItemNumber()==itemNumber) {
+				index=i;
+				break;
+			}
+		}
+		System.out.println("item number:"+itemList.get(index).getItemNumber());
+		System.out.println("item name:"+itemList.get(index).getItemName());
+		System.out.println("item quantity:"+itemList.get(index).getItemQuantity());
+	}
+	
 }
